211091fd45db39345e1111f49a9b02575286c3c2
hinali <hinali030@gmail.com>
2023-09-05 23:43:28 -0600

    added url_new.ejs and made get and post request for that

--- express_server.js
+++ express_server.js
@@ -3,6 +3,18 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true })); //middleware to parse the data in response body.
+
+function generateRandomString() {
+  const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    const random = Math.floor(Math.random() * char.length);
+    randomString += char[random];
+  }
+  console.log(randomString);
+  return randomString;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,6 +25,14 @@ app.get("/urls",(req, res) => {
 const templateVars = { urls : urlDatabase};
 res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+ console.log(req.body);
+ res.send("ok");
+})
 
 app.get("/urls/:id", (req, res) => {
   const shortID = req.params.id;
